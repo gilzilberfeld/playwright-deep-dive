966f123b9247877bf1f257a8efcf4c9213b3b52d
gilzilberfeld <gil.zilberfeld@gmail.com>
2025-10-20 10:06:41 +0300

    the hot reloading bug fix

--- app/api/a11/counter/state_management.ts
+++ app/api/a11/counter/state_management.ts
@@ -1,13 +1,19 @@
-let counter = 0;
+// We cast globalThis to `any` to be able to attach our custom property.
+// This is a standard pattern for persisting state in Next.js dev mode.
+const globalState = globalThis as any;
+
+if (globalState.counter_a11 === undefined) {
+  globalState.counter_a11 = 0;
+}
 
 export function incrementCounter() {
-  counter++;
+  globalState.counter_a11++;
 }
 
 export function setCounter(newCount: number) {
-  counter = newCount;
+  globalState.counter_a11 = newCount;
 }
 
 export function getCounter() {
-  return counter;
+  return globalState.counter_a11;
 }
